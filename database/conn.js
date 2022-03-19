b935cad1bcb94f54005318efd939f25f7812622d
Omkar Balaso Jarad <88423160+omkarjarad2002@users.noreply.github.com>
2022-03-19 13:03:51 +0530

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 const connection = async(req, res)=>{  
-    const responce = await mongoose.connect("mongodb+srv://omkar:omkar032002@cluster0.3t0u5.mongodb.net/mernstack?retryWrites=true&w=majority")
+    const responce = await mongoose.connect("mongodb+srv://omkar:omkar032002@cluster0.3t0u5.mongodb.net/sepblprojectF?retryWrites=true&w=majority")
 
     if(responce){
         console.log("Connection successfull !")

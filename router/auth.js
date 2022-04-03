cbb6ca85577c629acfa14f9c79b87c5ab01917dc
omkar <omkarjarad2019@gmail.com>
2022-04-03 12:04:54 +0530

    8

--- router/auth.js
+++ router/auth.js
@@ -84,7 +84,7 @@ router.post("/register", async (req, res)=>{
  
 //signIn route
 
-router.get("/login", async (req, res)=>{
+router.post("/login", async (req, res)=>{
     const {email , password} = req.body
 
     if(!email || !password){
